3a950f660d79e0950fa51d4dcfd0accc5d23318b
Kamil <kamilpier08@gmail.com>
2017-09-09 13:24:31 +0200

    Added deleting class

---
+++ Form_Universal_Delete.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ */
+class Form_Universal_Delete extends Form_Universal
+{
+  function __construct($form_name,$delete_btn,$column = 'id')
+  {
+    $this->form_name = $form_name;
+    if (isset($_POST[$delete_btn])) {
+      $sql = "DELETE FROM `$form_name` WHERE `$column` = '$_POST[$delete_btn]'";
+      if (conn()->query($sql) === TRUE) return 1; else return 0;
+    }
+  }
+}
